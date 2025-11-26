796014aa6df4eeedce62c1bc87e26a3af972f67a
Cyber-Tetsuki <costelojoven26@gmail.com>
2025-11-26 16:36:34 +0800

    first commit

---
+++ build/lib/cli/cli.py
@@ -0,0 +1,25 @@
+import sys
+from typer import Typer
+import copier
+
+app = Typer()
+
+
+@app.command()
+def create(project_name: str, dst_folder: str):
+    template_url = "https://github.com/Cyber-Tetsuki/mvp-flet"
+
+    default = {
+        "project_name": project_name,
+        "python_path": sys.executable,
+        "dst_path": dst_folder,
+    }
+
+    copier.run_copy(
+        src_path=template_url,
+        dst_path=dst_folder,
+        data=default,
+        unsafe=True,
+    )
+
+    print("MVP Flet App Created")
