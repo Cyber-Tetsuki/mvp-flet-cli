984b6ad9e3b7c2b66c6466129e9f0c058f60edb9
Cyber-Tetsuki <costelojoven26@gmail.com>
2025-11-26 18:38:43 +0800

    updated the cli.py added typer.Options

--- cli/cli.py
+++ cli/cli.py
@@ -4,29 +4,33 @@ import copier
 
 app = typer.Typer()
 
-#
-# @app.command()
-# def hello(name: str):
-#     print("Hello", name)
-#
-#
-# @app.command()
-# def create(
-#         project_name: str = typer.Option(..., "--proj-name", prompt=True),
-#         dst_folder: str = typer.Option(..., "--dist-path", prompt=True),
-# ):
-#     template_url = "https://github.com/Cyber-Tetsuki/mvp-flet"
-#     default = {
-#         "project_name": project_name,
-#         "python_path": sys.executable,
-#         "dst_path": dst_folder,
-#     }
-#
-#     copier.run_copy(
-#         src_path=template_url,
-#         dst_path=dst_folder,
-#         data=default,
-#         unsafe=True,
-#     )
-#
-#     print("MVP Flet App Created")
+
+@app.command()
+def hello(name: str):
+    print("Hello", name)
+
+
+@app.command()
+def create(
+        project_name: str = typer.Option(..., "--proj-name", prompt=True),
+        dst_folder: str = typer.Option(..., "--dist-path", prompt=True),
+):
+    template_url = "https://github.com/Cyber-Tetsuki/mvp-flet"
+    default = {
+        "project_name": project_name,
+        "python_path": sys.executable,
+        "dst_path": dst_folder,
+    }
+
+    copier.run_copy(
+        src_path=template_url,
+        dst_path=dst_folder,
+        data=default,
+        unsafe=True,
+    )
+
+    print("MVP Flet App Created")
+
+
+def main():
+    app()
