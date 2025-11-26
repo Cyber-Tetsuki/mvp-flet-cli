17d2c5dfc12924e31a6a979979a1de601214972d
Cyber-Tetsuki <costelojoven26@gmail.com>
2025-11-26 18:20:18 +0800

    update cli.py update toml and cli

--- cli/cli.py
+++ cli/cli.py
@@ -4,14 +4,16 @@ import copier
 
 app = typer.Typer()
 
+
 @app.command()
 def hello(name: str):
     print("Hello", name)
 
+
 @app.command()
 def create(
-    project_name: str = typer.Option(..., "--proj-name", prompt=True),
-    dst_folder: str = typer.Option(..., "--dist-path", prompt=True),
+        project_name: str = typer.Option(..., "--proj-name", prompt=True),
+        dst_folder: str = typer.Option(..., "--dist-path", prompt=True),
 ):
     template_url = "https://github.com/Cyber-Tetsuki/mvp-flet"
     default = {
@@ -28,3 +30,7 @@ def create(
     )
 
     print("MVP Flet App Created")
+
+
+def main():
+    app()
