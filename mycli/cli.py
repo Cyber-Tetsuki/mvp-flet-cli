5ba8fb66cc4963a6516c554b16a56615b0042592
Cyber-Tetsuki <costelojoven26@gmail.com>
2025-11-27 12:47:56 +0800

    added create view cli

--- mycli/cli.py
+++ mycli/cli.py
@@ -2,6 +2,7 @@ import sys
 import typer
 import copier
 import os
+import inflection
 
 app = typer.Typer()
 
@@ -31,6 +32,50 @@ def create(
     )
 
     print("MVP Flet App Created")
+
+
+def create_python_file(path: str, content: str):
+    try:
+        with open(path, "w") as f:
+            f.write(content)
+    except Exception as e:
+        print(e)
+
+
+@app.command()
+def create_view(view_name: str = typer.Option(..., "--view-name")):
+    cwd = os.getcwd()
+    class_name = inflection.camelize(view_name, uppercase_first_letter=True)
+    view_content = f"""
+    import flet as ft
+    from flet.core.types import MainAxisAlignment, CrossAxisAlignment
+    from model import EnvModel
+    from typing import Optional, TYPE_CHECKING
+    
+    if TYPE_CHECKING:
+        from presenter import {class_name}Presenter
+    
+    
+    class AppView:
+        def __init__(self, env: EnvModel):
+            self._env = env
+            self._presenter: Optional["{class_name}Presenter"] = None
+    """
+
+    presenter_content = f"""
+        from views import {class_name}View
+
+        class AppPresenter:
+            def __init__(self,view : {class_name}View):
+                self._view = view
+
+    """
+
+    view_path = cwd + "/views/" + view_name + "_view.py"
+    presenter_path = cwd + "/presenter/" + view_name + "_view.py"
+
+    create_python_file(view_path, view_content)
+    create_python_file(presenter_path, presenter_content)
 
 
 def main():
