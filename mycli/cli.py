a317410905974cfd21c8276bb2e86c6d3bbc47bc
Cyber-Tetsuki <costelojoven26@gmail.com>
2025-11-27 13:50:01 +0800

    added create view cli

--- mycli/cli.py
+++ mycli/cli.py
@@ -119,55 +119,77 @@ def create_view(name: str = typer.Option(..., "--name")):
 
 @app.command()
 def create_vp(name: str = typer.Option(..., "--name")):
+    create_view(name)
+    create_presenter(name)
+
+
+@app.command()
+def create_service(name: str = typer.Option(..., "--name")):
     cwd = os.getcwd()
-    presenter_class_name = inflection.camelize(f"{name}Presenter", uppercase_first_letter=True)
-    view_class_name = inflection.camelize(f"{name}View", uppercase_first_letter=True)
-    view_content = textwrap.dedent(
+    service_class_name = inflection.camelize(f"{name}Service", uppercase_first_letter=True)
+    repos_class_name = inflection.camelize(f"{name}Repos", uppercase_first_letter=True)
+    service_content = textwrap.dedent(
         f"""
-            import flet as ft
-            from flet.core.types import MainAxisAlignment, CrossAxisAlignment
-            from model import EnvModel
-            from typing import Optional, TYPE_CHECKING
+            from typing import TYPE_CHECKING
 
             if TYPE_CHECKING:
-                from presenter import {presenter_class_name}
+                from db.repository import {repos_class_name}
+            
+            
+            class {service_class_name}:
+                def __init__(self, setting_repos: '{repos_class_name}'):
+                    self._setting_repos = setting_repos
+
+          """
+    )
+
+    file_name = name + "_service.py"
+    view_path = cwd + "/services/"
+    file_path = view_path + file_name
+    create_python_file(file_path, service_content)
+    append_in_init(view_path, file_name, repos_class_name)
 
 
-            class {view_class_name}:
-                def __init__(self, env: EnvModel):
+@app.command()
+def create_repos(name: str = typer.Option(..., "--name")):
+    cwd = os.getcwd()
+    repos_class_name = inflection.camelize(f"{name}Repos", uppercase_first_letter=True)
+    repos_content = textwrap.dedent(
+        f"""
+            import traceback
+            from datetime import datetime
+            from typing import TYPE_CHECKING
+            from sqlalchemy import select, and_, update, insert
+            from sqlalchemy.orm import joinedload
+        
+            from db_models import Credentials, MatchActivity, SessionTable
+            from utils.logs import Logging
+            
+            if TYPE_CHECKING:
+                from model import EnvModel
+                from db import Database
+            
+            
+            class UserRepos:
+                def __init__(self, env: 'EnvModel', db: "Database"):
                     self._env = env
-                    self._presenter: Optional["{presenter_class_name}"] = None
-
-                def build(self):
-                    return ft.View()
+                    self._db = db
+                    self._logger = Logging()
+                    self._db.build()
             """
     )
 
-    presenter_content = textwrap.dedent(
-        f"""
-                 from views import {view_class_name}
+    file_name = name + "_repos.py"
+    view_path = cwd + "/db/repository/"
+    file_path = view_path + file_name
+    create_python_file(file_path, repos_content)
+    append_in_init(view_path, file_name, repos_class_name)
 
-                 class {presenter_class_name}:
-                     def __init__(self,view : {view_class_name}):
-                         self._view = view
 
-             """
-    )
-
-    """Presenter"""
-    presenter_file_name = name + "_presenter.py"
-    presenter_path = cwd + "/presenter/"
-    file_path = presenter_path + presenter_file_name
-    create_python_file(file_path, presenter_content)
-    append_in_init(presenter_path, presenter_file_name, presenter_class_name)
-
-    """View"""
-    view_file_name = name + "_view.py"
-    view_path = cwd + "/views/"
-    file_path = view_path + view_file_name
-    create_python_file(file_path, view_content)
-    append_in_init(view_path, view_file_name, view_class_name)
-
+@app.command()
+def create_rs(name: str = typer.Option(..., "--name")):
+    create_view(name)
+    create_presenter(name)
 
 def main():
     app()
